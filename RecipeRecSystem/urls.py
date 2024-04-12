33c18aa7bf7803fb3bba29d82bd896c31ce23c6b
Braden <braden.prestholdt@uwrf.edu>
2024-04-12 08:08:18 -0500

    Wrote view and mapped to url

--- RecipeRecSystem/urls.py
+++ RecipeRecSystem/urls.py
@@ -18,5 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('databaseManager/', include('databaseManager.urls')),
     path('admin/', admin.site.urls),
 ]
+
