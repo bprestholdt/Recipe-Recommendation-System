22b969a5dbb226db9c8223823053b7e371ecf361
Braden <braden.prestholdt@uwrf.edu>
2024-04-12 08:21:59 -0500

    Fixed views

--- RecipeRecSystem/urls.py
+++ RecipeRecSystem/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('databaseManager/', include('databaseManager.urls')),
+    path("", include("databaseManager.urls")),
     path('admin/', admin.site.urls),
 ]
 
