75f8ec02d5224cf13370fd5bf731b1f69bb0507f
Braden <braden.prestholdt@uwrf.edu>
2024-04-20 02:45:13 -0500

    Testing models in database and created "pages" app to hold html templates

--- databaseManager/models.py
+++ databaseManager/models.py
@@ -1,11 +1,29 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 #Models to define database layout and layout info to be stored
 
-#save username to database
-class saveUsername(models.Model):
-    username = models.CharField(max_length=50)
-    log_date = models.DateTimeField("date logged")
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    numRecipes = models.IntegerField(default = 0)
+
+class Recipe(models.Model):
+     title = models.CharField(max_length=100)
+     description = models.TextField()
+
+     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
+
+     def __str__(self):
+          return self.title
+    
+        
+    
+
 
