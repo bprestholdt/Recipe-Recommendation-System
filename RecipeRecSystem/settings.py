f7d167377457021a1fa2364a76f07f8b37bec8e3
Braden <braden.prestholdt@uwrf.edu>
2026-03-08 18:15:03 -0500

    Add CSRF_TRUSTED_ORIGINS for Railway HTTPS

--- RecipeRecSystem/settings.py
+++ RecipeRecSystem/settings.py
@@ -23,6 +23,8 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
